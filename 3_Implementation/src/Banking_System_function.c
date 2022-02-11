0bedb81959384b3755d8ee9b74a4c8293d5f7d9b
vikash0404 <62841254+vikash0404@users.noreply.github.com>
2022-02-11 12:15:58 +0530

    Add files via upload

---
+++ 3_Implementation/src/Banking_System_function.c
@@ -0,0 +1,12 @@
+
+
+void checkbalances(char*);
+void transferMoney(void);
+void display(char*);
+void person(char*);
+void login(void);
+void logins(void);
+void account(void);
+void accountCreated(void);
+void afterLogin(void);
+void logout(void);
